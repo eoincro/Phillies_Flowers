e89a493e45546d1965c3b439e59a1ac3ff9944d6
eoincro <blinkcro@gmail.com>
2017-12-07 21:12:41 +0000

    Add files via upload

---
+++ Phillies_Flowers1/src/main/java/com/cit/basepackage/entities/Orders.java
@@ -0,0 +1,47 @@
+package com.cit.basepackage.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Orders {
+	@Id
+	 int orderID;
+	 int quantity;
+	 int price;
+	 int productID;
+	 
+	 
+	public Orders(int orderID, int quantity, int price, int productID) {
+		super();
+		this.orderID = orderID;
+		this.quantity = quantity;
+		this.price = price;
+		this.productID = productID;
+	}
+	public int getOrderID() {
+		return orderID;
+	}
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+}
---
+++ Phillies_Flowers1/src/main/java/com/cit/basepackage/entities/Product.java
@@ -0,0 +1,63 @@
+package com.cit.basepackage.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Product {
+	@Id
+	int productID;
+	 int orderID;
+	 int quantity;
+	 int price;
+	 String productName;
+	 
+	public Product(int orderID, int quantity, int price, int productID, String productName) {
+		this.orderID = orderID;
+		this.quantity = quantity;
+		this.price = price;
+		this.productID = productID;
+		this.productName = productName;
+	}
+
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getProductID() {
+		return productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	 
+}
